fa3dbe13cb97eacb9d418300cdef1c91862cb6ab
jhonatan-dev <jhonatancastillosalvador@hotmail.com>
2020-01-22 17:04:09 -0500

    Agrupado práctica en una sola carpeta con la fecha de hoy y añadido una vista en el gateway-ms

---
+++ Practica22-01-2020/gateway-ms/src/main/java/com/everis/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.everis.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+	public String index() {
+		return "index";
+	}
+}
